7e4b8c240f994e6baf6ae89d564aa30cb1f72727
Mario Pastor <mariopastorlanchares@gmail.com>
2024-01-01 21:57:49 +0100

    feat - service firebase

---
+++ src/app/services/inverter-service.service.ts
@@ -0,0 +1,58 @@
+import {Injectable} from '@angular/core';
+import {AngularFireDatabase} from '@angular/fire/compat/database';
+import {map, Observable, of, switchMap, take} from 'rxjs';
+import {AngularFireAuth} from "@angular/fire/compat/auth";
+
+
+export interface Inverter {
+  acOutputActivePower: number;
+  batteryCapacity: number;
+  pvInputPower: number;
+  batteryVoltage: number;
+  totalOutActivePower: number;
+  batteryDischgCurrent: number;
+  totalChargingCurrent: number;
+  totalAcOutputActivePower: number;
+  timestr: string;
+}
+
+export const DEFAULT_INVERTER: Inverter = {
+  acOutputActivePower: 0,
+  batteryCapacity: 0,
+  pvInputPower: 0,
+  batteryVoltage: 0,
+  totalOutActivePower: 0,
+  batteryDischgCurrent: 0,
+  totalChargingCurrent: 0,
+  totalAcOutputActivePower: 0,
+  timestr: ''
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InverterService {
+  constructor(
+    private db: AngularFireDatabase,
+    private afAuth: AngularFireAuth,
+  ) {
+  }
+
+  getLatestInverterData(inverterId: string): Observable<Inverter> {
+    return this.afAuth.authState.pipe(
+      take(1),
+      switchMap(user => {
+        if (user) {
+          return this.db.list<{ [key: string]: Inverter }>(`${user.uid}`, ref =>
+            ref.orderByKey().limitToLast(1)
+          ).valueChanges().pipe(
+            map(records => records.length > 0 ? records[0][inverterId] : {...DEFAULT_INVERTER})
+          );
+        } else {
+          return of({...DEFAULT_INVERTER});
+        }
+      })
+    );
+  }
+
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ChartConfiguration, ChartType, ChartOptions} from 'chart.js';
+import {update} from "@angular/fire/database";
+import {DEFAULT_INVERTER, Inverter, InverterService} from "../services/inverter-service.service";
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
-  constructor() {}
 
+  ngOnInit() {
+    this.updateData();
+  }
+
+  inverter1: Inverter = DEFAULT_INVERTER;
+  inverter2: Inverter = DEFAULT_INVERTER;
+  date: string = '';
+
+  public batteryChartType: ChartType = 'doughnut';
+  public batteryChartData: ChartConfiguration['data'] = {
+//    labels: this.batteryLabels,
+    datasets: [{
+      data: [0, 100],
+      backgroundColor: ['#00c853', '#b9f6ca']
+    }]
+  };
+  public batteryChartOptions: ChartOptions = {
+    elements: {
+      arc: {
+        // Configura aquí las opciones de rotación y circunferencia
+       // rotation: Math.PI,
+      //  circumference: Math.PI
+      }
+    },
+    plugins: {
+      tooltip: {
+        callbacks: {
+          label: function(context) {
+            let label = context.dataset.data[context.dataIndex] + '%';
+            return label;
+          }
+        }
+      }
+    }
+  };
+
+
+
+
+  constructor(private inverterService: InverterService) {}
+
+  updateData() {
+    this.inverterService.getLatestInverterData('inverter_1').subscribe(data => {
+      console.log(data);
+      this.inverter1 = data;
+      this.date = data.timestr;
+    });
+    this.inverterService.getLatestInverterData('inverter_2').subscribe(data => {
+      this.inverter2 = data;
+    });
+  }
 }
